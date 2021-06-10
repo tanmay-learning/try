77b690030366e2426f6393cb4df73e77d955e93a
Tanmay Varade <42692334+tanmay6414@users.noreply.github.com>
2021-06-10 18:27:58 +0530

    Create try.tf

---
+++ a/try.tf
@@ -0,0 +1,19 @@
+provider "vault" {
+  address = "https://1a2c97adbe64.ngrok.io"
+  token = "s.EFQB11ZuW2f0F1Optc4xvDvH"
+}
+
+
+data "vault_generic_secret" "mq-userpass" {
+  #path = "kv/qak8s/vrp-pipelines/vrp-pipelines"
+  path = "kv/qak8s/namespaces/vrp-pipelines/vrp-pipelines"
+}
+
+
+locals {
+  username=data.vault_generic_secret.mq-userpass.data.username
+  password=data.vault_generic_secret.mq-userpass.data.password
+}
+output "a"{
+  value = local.password
+}
